6721879dcb5fcdc09d2f096ad0f2a40b9a365112
Costin Carabas <costin.carabas@gmail.com>
2021-02-05 11:51:58 +0200

    UJI: Plot similarities on UJI dataset
    In similarity.py, plot_similarities function, switch parameters:
        * meters - 3rd parameter
        * same_device - True/False
        * same_floor - True/False
        * same_month - True/False
    
    (Un)comment line 54-59 to plot only the desired similarities.
    Modify the plot intervals at lines 62-63.
    
    Signed-off-by: Costin Carabas <costin.carabas@gmail.com>

--- similarity.py
+++ similarity.py
@@ -4,7 +4,17 @@ from utils import *
 
 locations = []
 
-# AP positions
+"""
+    This function selects the locations of interest considering:
+        * physical distance
+        * type of device
+        * floor
+        * experiment date (month)
+    Next, *get_common_APs* selects only the common APs:
+        * APs_in_1 and APs_in_2 are the values of the common APs
+
+    You can plot 1 or more similarity methods by commenting lines 54-59.
+"""
 def plot_similarities(location):
     physical_plot = []
     cosine_plot = []
@@ -12,67 +22,100 @@ def plot_similarities(location):
     euclidean_plot = []
     minkowski_plot = []
     jaccard_plot = []
+    sorensen_plot = []
+    error_plot = []
+    #TODO loc[3]
+    APs_threshold = len(location[3])*0.1
 
     # Plot only for nearest locations
-    for l in get_nearest_locations(location, locations, 10):
+    for l in select_locations(location, locations, 20, same_device = True, \
+                                                       same_floor = True, \
+                                                       same_month = True):
 
-        # Skip the exact location
-        if physical_distance(l, location) == 0:
+        # Select only the APs that are common for both locations
+        [APs_in_1, APs_in_2] = get_common_APs(l, location)
+
+#         if len(APs_in_1) < APs_threshold
+#             continue
+
+        if not APs_in_1:
             continue
-
         # Create the similarity arrays
         physical_plot.append(physical_distance(l, location))
-        cosine_plot.append(cosine(tuple(l[3]), tuple(location[3])))
-        cityblock_plot.append(cityblock(tuple(l[3]), tuple(location[3])))
-        euclidean_plot.append(euclidean(tuple(l[3]), tuple(location[3])))
-        minkowski_plot.append(minkowski(tuple(l[3]), tuple(location[3]), 3))
-        jaccard_plot.append(jaccard(tuple(l[3]), tuple(location[3])))
+        cosine_plot.append(cosine(tuple(APs_in_1), tuple(APs_in_2)))
+        cityblock_plot.append(cityblock(tuple(APs_in_1), tuple(APs_in_2)))
+        euclidean_plot.append(euclidean(tuple(APs_in_1), tuple(APs_in_2)))
+        minkowski_plot.append(minkowski(tuple(APs_in_1), tuple(APs_in_2)))
+        jaccard_plot.append(jaccard(tuple(APs_in_1), tuple(APs_in_2)))
+        sorensen_plot.append(braycurtis(tuple(APs_in_1), tuple(APs_in_2)))
 
     # Plot similarities. Due to scalling issues, it's better to select only one
-    #plt.plot(physical_plot, cityblock_plot, 'o', label = "line 1")
-    #plt.plot(physical_plot, euclidean_plot, 'o', label = "line 1")
-    #plt.plot(physical_plot, minkowski_plot, 'o', label = "line 1")
-    #plt.plot(physical_plot, cosine_plot, 'o', label = "line 1")
-    plt.plot(physical_plot, jaccard_plot, 'o', label = "line 1")
+    # Select the similarity methods to be plotted
+    plt.plot(physical_plot, cityblock_plot, 'o', label = "City Block")
+    plt.plot(physical_plot, euclidean_plot, 'o', label = "Euclidean")
+    plt.plot(physical_plot, minkowski_plot, 'o', label = "Minkowski")
+    plt.plot(physical_plot, cosine_plot, 'o', label = "Cosine")
+    plt.plot(physical_plot, jaccard_plot, 'o', label = "Jaccard")
+    plt.plot(physical_plot, sorensen_plot, 'o', label = "Sorensen")
 
-    # Standardization: scale the similarity from -4 to +4
-    #plt.plot(physical_plot, preprocessing.scale(cityblock_plot), 'o', label = "line 1")
-    #plt.plot(physical_plot, preprocessing.scale(euclidean_plot), 'o', label = "line 2")
-    #plt.plot(physical_plot, preprocessing.scale(minkowski_plot), 'o', label = "line 3")
-    #plt.plot(physical_plot, preprocessing.scale(cosine_plot), 'o', label = "line 4")
-    #plt.plot(physical_plot, preprocessing.scale(jaccard_plot), 'o', label = "line 5")
+    # Plot
+    plt.xlim([0, 20])
+    plt.ylim([0,0.2])
+    plt.xlabel("Physical distance between provided location and all others (meters)")
+    plt.ylabel("Disimilarity Measurement")
 
     plt.legend()
     plt.show()
+
 
 def plot_common_APs(location):
     common_APs = []
     APs_only_1 = []
     APs_only_2 = []
     physical_distances = []
+    common_zero = []
+    APs_threshold = len(location[3])*0.05
 
-    for l in locations:
-        # Skip the exact location
-        if physical_distance(l, location) == 0:
-            continue
-        APs_only_1.append(get_common_APs(location, l)[0])
-        APs_only_2.append(get_common_APs(location, l)[1])
-        common_APs.append(get_common_APs(location, l)[2])
+    experiment_locations = select_locations(location, locations, meters = 20,
+                                            same_device = False,
+                                            same_floor = False,
+                                            same_month = False)
+
+    for l in experiment_locations:
+
+        # Skip locations
+        [APs_in_1, APs_in_2, APs_in_b] = get_number_APs(location, l)
+        #print([APs_in_1, APs_in_2, APs_in_b])
+        #if APs_in_1 < APs_threshold or APs_in_2 < APs_threshold:
+#         if APs_in_b < APs_in_1 * 0.5:
+#             continue
+
+        APs_only_1.append(APs_in_1)
+        APs_only_2.append(APs_in_2)
+        common_APs.append(APs_in_b)
         physical_distances.append(physical_distance(location, l))
 
-    print(common_APs)
+    # print(get_number_APs(location))
     plt.plot(physical_distances, common_APs, 'o', label = "Number of APs")
     #plt.plot(physical_distances, APs_only_1, 'o', label = "APs only in current location")
     #plt.plot(physical_distances, APs_only_2, 'o', label = "APs not in current location")
+
+    plt.xlabel("Physical distance between provided location and all others (meters)")
+    plt.ylabel("No. common Access Points")
     plt.legend()
     plt.show()
 
 
-loc1 = int(sys.argv[1])
 
-locations = load_dataset()
-norm_rss(locations)
+###### UJI #############3
+locations = load_dataset_uji()
+# norm_rss(locations)
 
-plot_common_APs(locations[loc1])
+# loc1 = int(sys.argv[1])
+#plot_common_APs(locations[loc1])
 
-#plot_similarities(locations[loc1])
+#TODO 
+for i in range(1, 4000, 500):
+    print("Location " + str(i))
+    plot_similarities(locations[i])
+#     plot_common_APs(locations[i])
--- utils.py
+++ utils.py
@@ -6,13 +6,19 @@ from scipy.spatial.distance import cosine
 from scipy.spatial.distance import cityblock
 from scipy.spatial.distance import minkowski
 from scipy.spatial.distance import jaccard
+from scipy.spatial.distance import braycurtis
 from sklearn import preprocessing
 import scipy.sparse as sp
 from collections import Counter
+import datetime
+import math
+import json
 
 
+min_rssi = -100
 
-def load_dataset():
+
+def load_dataset_uji():
     dev = []
     d = []
     x_y_z = []
@@ -46,7 +52,7 @@ def load_dataset():
             rsss = [int(numeric_string) for numeric_string in i]
             rss.append(rsss)
 
-    # locations = (device, date, [x,y,z], [AP1, AP2, ..])
+    # locations = (device, date, [x,y,z], [AP1, AP2, ..], avail_APs)
     for i in range(0,len(dev)):
         avail_rss = len(rss[i]) - Counter(rss[i])[100]
         location_tuple = (dev[i], d[i], x_y_z[i], rss[i], avail_rss)
@@ -58,12 +64,19 @@ def norm_rss(locations):
     for loc in locations:
         for rss in range(len(loc[3])):
             # Negative RSS turn to positive
-            if loc[3][rss] < 0:
-                loc[3][rss] = -loc[3][rss]
+            if loc[3][rss] < 0 and loc[3][rss] > -91:
+                # Linear values
+#                 loc[3][rss] = loc[3][rss] - min_rssi
+                # Exponential values
+                positive = loc[3][rss] - min_rssi
+                loc[3][rss] = pow(positive, math.e)/pow(-min_rssi, math.e)
+
+            else:
+                loc[3][rss] = 0
 
             # If not found, make 10.000
             if loc[3][rss] == 100:
-                loc[3][rss] = 100
+                loc[3][rss] = 0
     return locations
 
 # X Y Z positions
@@ -89,24 +102,61 @@ def similarity(location1, location2, method = None, minkowski_p=10):
     else:
         return method(tuple(location1[3]), tuple(location2[3]))
 
-def get_nearest_locations(location, locations, meters = 20):
-    nearest = []
+def select_locations(location, locations, meters, same_device = False,
+                     same_floor = False, same_month = False):
+    selection = []
     for l in locations:
-        if physical_distance(location, l) < meters:
-            nearest.append(l)
 
-    return nearest
+        # Remove the exact location (first parameter)
+        if physical_distance(l, location) == 0:
+            continue
 
-def get_common_APs(loc1, loc2):
+        # If same_device is set, check if devices are the same
+        if same_device and (l[0] != location[0]):
+            continue
+        # If same_floor is set, check if floors are the same
+        if same_floor and (l[2][2] != location[2][2]):
+            continue
+
+        # If same_month is set, check if months are the same
+        if same_month and \
+            (datetime.datetime.strptime(l[1], "%Y-%m-%d %H:%M:%S").month !=
+             datetime.datetime.strptime(location[1], "%Y-%m-%d %H:%M:%S").month):
+            continue
+
+        # Check if 
+        if meters and (physical_distance(location, l) > meters):
+            continue
+
+        selection.append(l)
+
+    return selection
+
+def get_number_APs(loc1, loc2):
     count_b = 0
     count_1 = 0
     count_2 = 0
     for i in range(len(loc1[3])):
         if loc1[3][i] != 100 and loc2[3][i] != 100:
+            #print(str(loc1[3][i])+", "+str(loc2[3][i]))
             count_b += 1
-        if loc1[3][i] == 100 and loc2[3][i] != 100:
+            #print(str(loc1[3][i])+", "+str(loc2[3][i]))
+        if loc2[3][i] != 100:
             count_2 += 1
-        if loc1[3][i] != 100 and loc2[3][i] == 100:
+            #print(str(loc1[3][i])+", "+str(loc2[3][i]))
+        if loc1[3][i] != 100:
             count_1 += 1
 
+    #print(str(count_1)+", "+str(count_2)+", "+str(count_b))
     return [count_1, count_2, count_b]
+
+def get_common_APs(loc1, loc2):
+    APs_in_1 = []
+    APs_in_2 = []
+
+    for i in range(len(loc1[3])):
+        if loc1[3][i] != 0 and loc2[3][i] != 0:
+            APs_in_1.append(loc1[3][i])
+            APs_in_2.append(loc2[3][i])
+
+    return [APs_in_1, APs_in_2]
