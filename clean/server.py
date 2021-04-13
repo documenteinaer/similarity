515da506699a684cad92234cfac616b0568ef069
Costin Carabas <costin.carabas@gmail.com>
2021-04-13 16:45:28 +0300

    clean/server.py: Add plotting functionality
    Signed-off-by: Costin Carabas <costin.carabas@gmail.com>

--- clean/server.py
+++ clean/server.py
@@ -2,6 +2,8 @@
 import sys
 from server_utils import *
 from preprocessing import *
+import matplotlib.pyplot as plt
+
 
 # Preprocessing Phase; it will result a file "p_json_name"
 preprocessing(sys.argv[1])
@@ -13,3 +15,20 @@ collections = load_collections(input_json)
 # Compare two locations
 similarity = compare_locations(collections[3], collections[4])
 print("Similarity :", similarity)
+
+
+
+
+############## Plotting purposes only ###################
+
+similarity_array = []
+for c in range(len(collections)):
+    similarity_array.append(compare_locations(collections[3], collections[c]))
+
+plt.plot(similarity_array, 'o', label = "Similarity (Average)")
+plt.xlabel("Nr. crt.")
+plt.ylabel("Similarity Measurement")
+plt.legend()
+plt.show()
+
+
