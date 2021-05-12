32398895309ad8257110dc3e204a23357bc5465c
Costin Carabas <costin.carabas@gmail.com>
2021-05-12 14:57:44 +0300

    Add comparison methods in compare_locations.py
    Mean, Median, Average, Max, Std
    
    Signed-off-by: Costin Carabas <costin.carabas@gmail.com>

--- compare_locations.py
+++ compare_locations.py
@@ -7,6 +7,7 @@ from utils import *
 def compare_locations(c1, c2, simil_method = braycurtis,  method = 'Average'):
     rssi1 = []
     rssi2 = []
+    w = [] #weight for braycurtis
     wifi1 = c1['fingerprints']['wifi']
     wifi2 = c2['fingerprints']['wifi']
 
@@ -31,10 +32,32 @@ def compare_locations(c1, c2, simil_method = braycurtis,  method = 'Average'):
         if method == 'Average':
             rssi1.append(np.average(adjust_rssi(wifi1[ap]['rssi'])))
             rssi2.append(np.average(adjust_rssi(wifi2[ap]['rssi'])))
+            w.append(min(len(wifi1[ap]['rssi']), len(wifi2[ap]['rssi'])))
+
+        if method == 'Median':
+            rssi1.append(np.median(adjust_rssi(wifi1[ap]['rssi'])))
+            rssi2.append(np.median(adjust_rssi(wifi2[ap]['rssi'])))
+
+        if method == 'Mean':
+            rssi1.append(np.mean(adjust_rssi(wifi1[ap]['rssi'])))
+            rssi2.append(np.mean(adjust_rssi(wifi2[ap]['rssi'])))
+
+        if method == 'Std':
+            rssi1.append(np.std(adjust_rssi(wifi1[ap]['rssi'])))
+            rssi2.append(np.std(adjust_rssi(wifi2[ap]['rssi'])))
+
+        if method == 'Max':
+            rssi1.append(np.max(adjust_rssi(wifi1[ap]['rssi'])))
+            rssi2.append(np.max(adjust_rssi(wifi2[ap]['rssi'])))
+
+
 
         if method == 'Tempered':
             rss_1.append(np.average(rssi_v[index][key]) * random.uniform(0.8, 1.2))
             rss_2.append(np.average(rssi_v[r][key]) * random.uniform(0.8, 1.2))
 
+
+#     if simil_method == braycurtis:
+#         return simil_method(tuple(rssi1), tuple(rssi2), w)
 
     return simil_method(tuple(rssi1), tuple(rssi2))
