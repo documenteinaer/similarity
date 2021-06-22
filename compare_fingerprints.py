a4b1221be77cbd2b8f056dce5f45076fa771ece6
laura <laura.cristina04@gmail.com>
2021-06-22 11:52:45 +0300

    compare_fingerprints module

---
+++ compare_fingerprints.py
@@ -0,0 +1,128 @@
+import numpy as np
+from scipy.spatial.distance import braycurtis
+from scipy.stats import ks_2samp, chisquare
+import copy 
+from utils import *
+
+
+def precalculate_fingerprints(c): # c is a collection
+    """ creates new fields to speed up distance comparisons 
+          c.ufingerprint['wifirssi'][mac] = average dBm 
+    """
+    ufingerprint = {}
+    ufingerprint['wifirssi'] = {}
+    for f in c['fingerprints']:
+        if not "wifi" in f.keys():
+            continue
+        for mac in f["wifi"].keys():
+            avg_pow = np.average(f["wifi"][mac]['rssi']) # each fingerprint (direction) counts the same
+            if not mac in ufingerprint["wifirssi"].keys():
+                ufingerprint["wifirssi"][mac] = [avg_pow] 
+            else:
+                ufingerprint["wifirssi"][mac].append(avg_pow)
+    for mac in ufingerprint["wifirssi"].keys():
+        ufingerprint["wifirssi"][mac] = \
+            np.average(adjust_rssi(ufingerprint["wifirssi"][mac])) #each fingerprint (direction) counts the same
+    c['ufingerprint'] = ufingerprint
+                
+def merge_wifi_fingerprints(flist):
+    if len(flist) == 1:
+        return flist[0]
+    fingerprint = copy.deepcopy(flist[0])
+    for f2 in copy.deepcopy(flist[1:]):
+        if not "wifi" in f2.keys():
+            continue
+        for mac in f2["wifi"].keys():
+            if not mac in fingerprint["wifi"].keys():
+                fingerprint["wifi"][mac] = f2["wifi"][mac]
+            else:
+                fingerprint["wifi"][mac]['rssi'].extend(f2["wifi"][mac]['rssi'])
+            fingerprint["wifi"][mac]['rssi'].sort()    
+    return fingerprint
+
+def compare_fingerprints(c1, c2, simil_method = braycurtis,  selection = 'Average', dif = True):
+    # precalculated with average, adjust_rssi  
+    wifi1 = c1['ufingerprint']['wifirssi']
+    wifi2 = c2['ufingerprint']['wifirssi']      
+    
+    common_aps = list(set(wifi1.keys()) & set(wifi2.keys()))
+    # No APs in common -> similarity = 1
+    if not common_aps:
+        return 1.0
+
+    if len(common_aps) * 5 <= len(wifi1.keys()) or len(common_aps) < 3:
+        return 1.0
+
+    aps1 = set(wifi1.keys()) - set(common_aps)
+    aps2 = set(wifi2.keys()) - set(common_aps)
+    rssi1 = np.empty(len(common_aps) + len(aps1) + len(aps2), dtype=float)
+    rssi2 = np.empty(len(common_aps) + len(aps1) + len(aps2), dtype=float)
+    nap = 0   #  
+    
+    if selection == 'First':
+        for ap in common_aps:
+            rssi1.append(wifi1[ap]['rssi'][0])
+            rssi2.append(wifi2[ap]['rssi'][0])
+
+        # Make an average of all RSSI values
+    if selection == 'Average':
+        for ap in common_aps:
+            rssi1[nap] = wifi1[ap]
+            rssi2[nap] = wifi2[ap]
+            nap = nap + 1
+            
+    if selection == 'Median':
+        for ap in common_aps:
+            rssi1.append(np.median(adjust_rssi(wifi1[ap]['rssi'])))
+            rssi2.append(np.median(adjust_rssi(wifi2[ap]['rssi'])))
+
+    if selection == 'Mean':
+        for ap in common_aps:
+            rssi1.append(np.mean(adjust_rssi(wifi1[ap]['rssi'])))
+            rssi2.append(np.mean(adjust_rssi(wifi2[ap]['rssi'])))
+
+    if selection == 'Std':
+        for ap in common_aps:
+            rssi1.append(np.std(adjust_rssi(wifi1[ap]['rssi'])))
+            rssi2.append(np.std(adjust_rssi(wifi2[ap]['rssi'])))
+
+    if selection == 'Max':
+        for ap in common_aps:
+            rssi1.append(np.max(adjust_rssi(wifi1[ap]['rssi'])))
+            rssi2.append(np.max(adjust_rssi(wifi2[ap]['rssi'])))
+            
+    if selection == 'KS':
+        for ap in common_aps:
+            _, p = ks_2samp(wifi1[ap]['rssi'], wifi2[ap]['rssi'])
+            rssi1.append(p)
+            rssi2.append(1.0)
+
+    if (dif == True) and (selection != 'KS'):
+        if len(rssi1) > nap:
+            rssi1[nap] = rssi1[0]
+            rssi2[nap] = rssi2[0]
+        rssi1 = np.diff(rssi1, append=rssi1[0])
+        rssi2 = np.diff(rssi2, append=rssi1[0])   
+
+   
+    """
+    p1 = np.mean(list(map(lambda a: np.mean(wifi1[a]['rssi']), wifi1.keys())))
+    p2 = np.mean(list(map(lambda a: np.mean(wifi2[a]['rssi']), wifi2.keys())))
+    pc1 = np.mean(list(map(lambda a: np.mean(wifi1[a]['rssi']), common_aps)))
+    pc2 = np.mean(list(map(lambda a: np.mean(wifi2[a]['rssi']), common_aps)))
+    po1 = np.mean(list(map(lambda a: np.mean(wifi1[a]['rssi']), aps1)))
+    po2 = np.mean(list(map(lambda a: np.mean(wifi2[a]['rssi']), aps2)))        
+    iou = len(common_aps) / \
+        (len(aps1) + len(aps2) + len(common_aps))
+    """
+        
+    for ap in aps1:
+        rssi1[nap] = 1.0*wifi1[ap]
+        rssi2[nap] = adjust_rssi(-95.0)[0]
+        nap = nap + 1
+    for ap in aps2:
+        rssi2[nap] = 1.0*wifi2[ap]
+        rssi1[nap] = adjust_rssi(-95.0)[0]
+        nap = nap + 1
+        
+    return simil_method(rssi1, rssi2)
